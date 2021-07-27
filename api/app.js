56e4df1fec750a3689a87f2616a38a8eb9b9a5c3
gjgd <gjgd@protonmail.com>
2021-07-27 04:33:34 +0000

    feat: add error handling

--- api/app.js
+++ api/app.js
@@ -13,67 +13,75 @@ const pageSize = 20;
 
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-  res.status(200).send('Hi');
-});
-
 app.get('/transactions/:blockHeight', async (req, res) => {
-  const { blockHeight } = req.params;
-  if (blockHeight) {
-    const params = {
-      TableName: process.env.TABLE_NAME,
-      Key: {
-        network: 'mainnet',
-        blockHeight: Number(blockHeight),
-      },
-    };
-    const record = await docClient.get(params).promise();
-    if (record.Item) {
-      res.status(200).send(record.Item);
-    } else {
-      res.sendStatus(404);
+  try {
+    const { blockHeight } = req.params;
+    if (blockHeight) {
+      const params = {
+        TableName: process.env.TABLE_NAME,
+        Key: {
+          network: 'mainnet',
+          blockHeight: Number(blockHeight),
+        },
+      };
+      const record = await docClient.get(params).promise();
+      if (record.Item) {
+        res.status(200).send(record.Item);
+      } else {
+        res.sendStatus(404);
+      }
     }
+    res.sendStatus(400);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: 'Something broke! Check the logs' })
   }
-  res.sendStatus(400);
 });
 
 app.get('/transactions', async (req, res) => {
-  const { before } = req.query;
-  if (before) {
-    const params = {
-      TableName: process.env.TABLE_NAME,
-      KeyConditionExpression: 'network = :network AND blockHeight < :blockHeight',
-      ExpressionAttributeValues: {
-        ':network': 'mainnet',
-        ':blockHeight': Number(before),
-      },
-      // Descending order of block height (most recent transaction first)
-      ScanIndexForward: false,
-      Limit: pageSize,
-    };
-    const record = await docClient.query(params).promise();
-    if (record.Items) {
-      res.status(200).send(record.Items);
+  try {
+    const { before } = req.query;
+    if (before) {
+      const params = {
+        TableName: process.env.TABLE_NAME,
+        KeyConditionExpression: 'network = :network AND blockHeight < :blockHeight',
+        ExpressionAttributeValues: {
+          ':network': 'mainnet',
+          ':blockHeight': Number(before),
+        },
+        // Descending order of block height (most recent transaction first)
+        ScanIndexForward: false,
+        Limit: pageSize,
+      };
+      const record = await docClient.query(params).promise();
+      if (record.Items) {
+        res.status(200).send(record.Items);
+      } else {
+        res.sendStatus(404);
+      }
     } else {
-      res.sendStatus(404);
+      const params = {
+        TableName: process.env.TABLE_NAME,
+        KeyConditionExpression: 'network = :network',
+        ExpressionAttributeValues: {
+          ':network': 'mainnet'
+        },
+        // Descending order of block height (most recent transaction first)
+        ScanIndexForward: false,
+        Limit: pageSize,
+      };
+      const record = await docClient.query(params).promise();
+      if (record.Items) {
+        res.status(200).send(record.Items);
+      } else {
+        res.sendStatus(404);
+      }
     }
-  } else {
-    const params = {
-      TableName: process.env.TABLE_NAME,
-      KeyConditionExpression: 'network = :network',
-      ExpressionAttributeValues: {
-        ':network': 'mainnet'
-      },
-      // Descending order of block height (most recent transaction first)
-      ScanIndexForward: false,
-      Limit: pageSize,
-    };
-    const record = await docClient.query(params).promise();
-    if (record.Items) {
-      res.status(200).send(record.Items);
-    } else {
-      res.sendStatus(404);
-    }
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ message: 'Something broke! Check the logs' })
   }
 });
 
